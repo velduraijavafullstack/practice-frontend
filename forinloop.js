ea63eec384b61c988615ef928f1331714be9920c
velduraijavafullstack <veldurai232@gmail.com>
2023-11-05 21:33:12 +0530

    loops

--- forinloop.js
+++ forinloop.js
@@ -1,3 +1,15 @@
+ places = {
+city1 : "chennai",
+city2 : "Benglore",
+city3 : "Tirunelveli"
+
+}
+for(place in places)
+{
+  console.log(places[place]);
+}
+
+
 let names = ["arun", "bala","charan","david","edison","fathima"]
 
 for(name in names){
--- forofloop.js
+++ forofloop.js
@@ -3,3 +3,8 @@ var names =["arun", "bala","charan","david","edison","fathima"]
 for(name of names){
   console.log(name);
 }
+
+var str = "String"
+for(s of str){
+  console.log(s);
+}
